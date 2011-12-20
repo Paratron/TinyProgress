2c6408ff8a1ca3ff52e9622c644af25c8fee14a5
Christian Engel <christian.engel@wearekiss.com>
2011-12-20 10:27:20 +0100

    Put together a working example. Open "/?example" in your browser to test it.

--- index.php
+++ index.php
@@ -1,4 +1,51 @@
 <?php
 foreach($_GET as $progress_file=>$v){break;}
 
-echo $progress_file;
+$progress_file = str_replace('..', '', $progress_file);
+
+$dta = @file_get_contents('lib/progress/'.$progress_file.'.json');
+
+if(!$dta){
+    die('Progress not found.');
+}
+
+function render($dta){
+    $project = $dta['project'];
+    $milestones = $dta['milestones'];
+
+    foreach($milestones as $k=>$v){
+        $m_progress = 0;
+        foreach($v as $k2=>$v2){ //Features
+            $f_progress = 0;
+            foreach($v2['tasks'] as $k3=>$v3){ //Tasks
+                $t_title = '';
+                $t_progress = '';
+                if(is_int($v3)){
+                    $t_title = $v3.'%';
+                    $t_progress = $v3;
+                } else {
+                    $p = explode('/', $v3);
+                    $ps = 100 / $p[1];
+                    $t_progress = round($p[0] * $ps);
+                    $t_title = $p[0] . ' of ' . $p[1];
+                }
+                $f_progress += $t_progress;
+
+                $milestones[$k][$k2]['tasks'][$k3] = array(
+                    'label' => $t_title,
+                    'progress' => $t_progress
+                );
+            }
+
+            if(count($v2['tasks'])) $f_progress = round($f_progress / count($v2['tasks']));
+            $milestones[$k][$k2]['__progress__'] = $f_progress;
+            $m_progress += $f_progress;
+        }
+        if(count($v)) $m_progress = round($m_progress / count($v));
+        $milestones[$k]['__progress__'] = $m_progress;
+    }
+
+    include('lib/template.php');
+}
+
+render(json_decode($dta, TRUE));
---
+++ lib/template.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="favicon.ico" rel="shortcut icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="lib/css/style.css" type="text/css" rel="stylesheet" media="screen">
+    <title>TinyProgress</title>
+</head>
+<body>
+    <article>
+        <div class="meta">
+            <h1><?= $project['name'] ?></h1>
+            <p><?= nl2br($project['description']) ?></p>
+        </div>
+
+        <? foreach($milestones as $title => $milestone):
+        if($title == '__progress__') break; ?>
+        <section class="milestone">
+            <h2><?= $title ?></h2>
+            <div class="progress"><span class="bar orange" style="width: <?=$milestone['__progress__']?>%"></span><span class="label"><?=$milestone['__progress__']?>%</span></div>
+
+            <? foreach($milestone as $title2 => $feature):
+            if($title2 == '__progress__') break; ?>
+            <div class="feature">
+                <h3><?= $title2 ?></h3>
+                <div class="progress"><span class="bar yellow" style="width: <?=$feature['__progress__']?>%"></span><span class="label"><?=$feature['__progress__']?>%</span></div>
+                <? foreach($feature['tasks'] as $title3 => $task): ?>
+                <div class="task">
+                    <span class="label"><?= $title3 ?></span>
+                    <div class="progress"><span class="bar green" style="width: <?=$task['progress']?>%"></span><span class="label"><?= $task['label']?></span></div>
+                </div>
+                <? endforeach; ?>
+            </div>
+            <? endforeach; ?>
+
+        </section>
+    <? endforeach; ?>
+    </article>
+</body>
+</html>
